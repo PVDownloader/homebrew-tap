ae6d3dea6f1e066293204935188af9e5dbcdd56b
Berke Alp Çalışkan <berkealp@tutamail.com>
2026-09-12 21:30:12 +0300

    fix: repo URL

--- Casks/pvdownloader.rb
+++ Casks/pvdownloader.rb
@@ -5,7 +5,7 @@ cask "pvdownloader" do
   sha256 arm:   "e08b05d5653ef8c83b2b87684d9a258f430e3592e519e5fec1f7a5462f4e87ee",
          intel: "8efba793b45d67b5f3a8247153b13a1859cc59c925a7a1d14d99cf99ed5ac6ec"
 
-  url "https://github.com/berkealp/pvdownloader/releases/download/v#{version}/PVDownloader-osx-#{arch}-Setup.pkg"
+  url "https://github.com/Berke-Alp/pvdownloader-releases/releases/download/v#{version}/PVDownloader-osx-#{arch}-Setup.pkg"
   name "PVDownloader"
   desc "High-speed, ad-free media archiving tool"
   homepage "https://pvdownloader.com"
